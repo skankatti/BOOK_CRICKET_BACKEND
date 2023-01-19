c785db7c0be0f47da75bf00107fc911ebba5fd87
LRM-00092\Balasaheb.Rathod <balasaheb.rathod@leftrightmind.com>
2023-01-19 17:09:56 +0530

    Removed Unnecessary comments from RandomRungeneratorServ.java

--- src/main/java/cricket/game/service/RandomRunGenSERV.java
+++ src/main/java/cricket/game/service/RandomRunGenSERV.java
@@ -10,7 +10,6 @@ public class RandomRunGenSERV {
 	int total;
 	int x;
 	
-
 	public int runGenerator() {
 		Random random = new Random();
 		//if(ball>-1)
@@ -32,33 +31,27 @@ public class RandomRunGenSERV {
 		 case 0: ans = ShowComZero();
 		 return ans;
 		 case 1: return "One run";
-		 		
-		// break;
-		 case 2: //System.out.println("Two Runs");
+		 
+		 case 2: 
 		 return "Two Runs";
-		// break;
-		 case 3:  return "Three Runs";//System.out.println("Three Runs");
-		// break;
+	
+		 case 3:  return "Three Runs";
+
 		 case 4: ans = ShowComFour();
-			 return ans;//System.out.println(ShowComFour());
-		 //break;
-		 case 5: return "Leg by One run";//System.out.println("Hit on helmet");
-		// break;	
+			 return ans;
+
+		 case 5: return "Leg by One run";
+		
 		 case 6: ans =  ShowComSix();
-			 return ans ;//System.out.println(ShowComSix());
+			 return ans ;
 		 case 7: ans =  ShowComSeven();
 		 return ans ;
-		 
-		 
-		// break;
 		 }
-		 
-		// strans= x+"hello";
 
 		return ans;
 	}
 	public String ShowComZero(){
-		String[] arr = { "LBW", "Clean Bold", "Catch out" };
+		String[] arr = { "LBW", "Clean bowled", "Catch out" };
 		Random random = new Random();
 
 		// randomly selects an index from the arr
@@ -70,53 +63,32 @@ public class RandomRunGenSERV {
 		return arr[select];
 	}
 	public String ShowComFour(){
-		String [] arr = {"Four Runs", "Grate four", "Super four", "Splendid shot for four"};
+		String [] arr = {"Four Runs", "Great four", "Super four", "Splendid shot for four"};
 	    Random random = new Random();
-
-	    // randomly selects an index from the arr
 	    int select = random.nextInt(arr.length); 
 
-	    // prints out the value at the randomly selected index
-	  //  System.out.println("Random String selected: " + arr[select]);
-	    
 		return arr[select];
 	}
 	public String ShowComSeven(){
 	    String [] arr = {"Oh it's wide", "Oh it's no ball"};
 	    Random random = new Random();
-
-	    // randomly selects an index from the arr
-	    int select = random.nextInt(arr.length); 
-
-	    // prints out the value at the randomly selected index
-	  //  System.out.println("Random String selected: " + arr[select]);
-	    
-	    return arr[select];
+    int select = random.nextInt(arr.length); 
+    return arr[select];
 	}
 	
 	public String ShowComSix(){
 		String [] arr = {"Six Runs", "What a Splendid Six", "Super Six", " What a long six!"};
 	    Random random = new Random();
-
-	    // randomly selects an index from the arr
 	    int select = random.nextInt(arr.length); 
 
-	    // prints out the value at the randomly selected index
-	  //  System.out.println("Random String selected: " + arr[select]);
-	    
 		return arr[select];
 	}
 	
 	public String ShowComFive(){
-	    String [] arr = {"Leg by One run", "Bye's one Run"};
+	    String [] arr = {"Leg by One run"};
 	    Random random = new Random();
+	    	int select = random.nextInt(arr.length); 
 
-	    // randomly selects an index from the arr
-	    int select = random.nextInt(arr.length); 
-
-	    // prints out the value at the randomly selected index
-	  //  System.out.println("Random String selected: " + arr[select]);
-	    
 	    return arr[select];
 	}
 	
